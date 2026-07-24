0bfd143dd7bf458c6c49371f68f43743378bd4ab
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2026-07-24 10:31:21 -0400

    git add . git commit -m "chore: establish UAP monorepo foundation"

---
+++ backend/uap-server/src/test/java/com/devinolabs/uap/UapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinolabs.uap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
